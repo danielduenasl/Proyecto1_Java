d0a55ef66ea3bd913f152c383917c023b3cc6f35
Daniel Dueñas <97635459+danielduenasl@users.noreply.github.com>
2024-08-23 22:26:52 -0600

    grados
    set y get

--- src/data/Grades.java
+++ src/data/Grades.java
@@ -10,9 +10,52 @@ package data;
  */
 public class Grades extends Student{
     
+    public String subject;
     public float note1;
     public float note2;
     public float note3;
     public float note4;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public float getNote1() {
+        return note1;
+    }
+
+    public void setNote1(float note1) {
+        this.note1 = note1;
+    }
+
+    public float getNote2() {
+        return note2;
+    }
+
+    public void setNote2(float note2) {
+        this.note2 = note2;
+    }
+
+    public float getNote3() {
+        return note3;
+    }
+
+    public void setNote3(float note3) {
+        this.note3 = note3;
+    }
+
+    public float getNote4() {
+        return note4;
+    }
+
+    public void setNote4(float note4) {
+        this.note4 = note4;
+    }
+    
+    
     
 }
